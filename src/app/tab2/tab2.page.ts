b496bbc37e0a8cfc92c76e43f1bc31e3f45f2fd1
ridh <rea.mazlan@gmail.com>
2022-01-28 15:02:16 +0800

    QR Scanner JS (worked)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,85 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  scanActive = false;
+  scanResult = null;
+
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+
+  videoElement: any;
+  canvasElement: any;
+  canvasContext: any;
+
   constructor() {}
 
+  ngAfterViewInit() {
+    this.videoElement = this.video.nativeElement;
+    this.canvasElement  = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+  }
+
+  async startScan(){
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment'}
+    });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  scan() {
+    console.log('SCAN');
+
+    if(this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA){
+      this.scanActive = true;
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+    
+      console.log('code: ', code);
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+
+      } else {
+        if (this.scanActive) {
+
+        }
+        requestAnimationFrame(this.scan.bind(this));
+      }
+
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  stopScan(){
+    this.scanActive = false;
+  }
+
+  reset(){
+    this.scanResult = null;
+  }
 }
